1a5371fe1562efcc00d7727a8d31136bc89a97dc
MuhammadJufri <sayyidmuhammad878@gmail.com>
2022-02-22 14:28:54 +0700

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/Components/**/*.{js,ts,jsx,tsx}',
-    './src/Layout/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layout/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
